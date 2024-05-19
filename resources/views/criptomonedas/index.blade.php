65de8475d1a43dad6ce4ff2cb82c095d4ff38468
Claudio_Torrez_De_Paz <claudiotorrezkk@gmail.com>
2024-05-18 21:02:58 -0600

    Mi mensaje mio insertado

--- resources/views/criptomonedas/index.blade.php
+++ resources/views/criptomonedas/index.blade.php
@@ -1,3 +1,5 @@
 Adios juas xd
 
-Buenas noches a todos son las 8:21 de la noche 
+Buenas noches a todos son las 8:21 de la noche
+
+Miguel Hermida del 601 es una roba maridos y no paga las tandas, pero bien que pelea su número de tanda
